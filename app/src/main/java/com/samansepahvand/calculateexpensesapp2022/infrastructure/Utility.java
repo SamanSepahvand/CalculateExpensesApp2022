36c683b153ccdff72dd76a80b0be29fd8f6307b3
SamanSepahvand <shantav75@gmail.com>
2022-04-29 12:45:10 +0430

    season 3 - section 1

--- app/src/main/java/com/samansepahvand/calculateexpensesapp2022/infrastructure/Utility.java
+++ app/src/main/java/com/samansepahvand/calculateexpensesapp2022/infrastructure/Utility.java
@@ -3,6 +3,14 @@ package com.samansepahvand.calculateexpensesapp2022.infrastructure;
 import android.animation.ObjectAnimator;
 import android.widget.ImageView;
 
+import com.samansepahvand.calculateexpensesapp2022.R;
+import com.samansepahvand.calculateexpensesapp2022.helper.CalendarTool;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public   class Utility {
 
 
@@ -13,6 +21,57 @@ public   class Utility {
         animator.setDuration(200);
         animator.start();
     }
+
+
+    public static int ChangeIconAddExpenses(int count) {
+        if (count == 1) return R.drawable.ic_baseline_check_24;
+        return R.drawable.ic_baseline_arrow_back_ios_24;
+
+    }
+
+
+
+    public static String getIranianDateInt() {
+        CalendarTool tool = new CalendarTool();
+
+        String date = String.format(Locale.US, "%d/%02d/%02d",
+                tool.getIranianYear(), tool.getIranianMonth(),
+                tool.getIranianDay());
+        return date;
+    }
+
+
+    public static String getIranianDateCustom(Calendar calendar) {
+        Calendar cNew=Calendar.getInstance();
+        CalendarTool tool = new CalendarTool(calendar);
+        String date = String.format(Locale.US, "%d/%02d/%02d %02d:%02d:%02d",
+                tool.getIranianYear(), tool.getIranianMonth(),
+                tool.getIranianDay(), cNew.get(Calendar.HOUR_OF_DAY),
+                cNew.get(Calendar.MINUTE), cNew.get(Calendar.SECOND));
+        return date;
+    }
+
+    public static String getIranianDate() {
+
+        CalendarTool tool = new CalendarTool();
+        Calendar cal = Calendar.getInstance();
+
+        String date = String.format(Locale.US, "%d/%02d/%02d %02d:%02d:%02d",
+                tool.getIranianYear(), tool.getIranianMonth(),
+                tool.getIranianDay(), cal.get(Calendar.HOUR_OF_DAY),
+                cal.get(Calendar.MINUTE), cal.get(Calendar.SECOND));
+        return date;
+    }
+
+
+    public static int GetActionDate() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+        Calendar mDate = Calendar.getInstance();
+        return Integer.parseInt(dateFormat.format(mDate.getTime()));
+    }
+
+
+
 
 
 }
--- app/src/main/java/com/samansepahvand/calculateexpensesapp2022/ui/fragment/AddInvoicesFragment.java
+++ app/src/main/java/com/samansepahvand/calculateexpensesapp2022/ui/fragment/AddInvoicesFragment.java
@@ -1,21 +1,53 @@
 package com.samansepahvand.calculateexpensesapp2022.ui.fragment;
 
+import static android.content.ContentValues.TAG;
+import static com.samansepahvand.calculateexpensesapp2022.infrastructure.Utility.ChangeIconAddExpenses;
+
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.text.Editable;
+import android.text.Html;
+import android.text.Spanned;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.samansepahvand.calculateexpensesapp2022.R;
+import com.samansepahvand.calculateexpensesapp2022.db.PriceType;
+import com.samansepahvand.calculateexpensesapp2022.infrastructure.Utility;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import ir.hamsaa.persiandatepicker.PersianDatePickerDialog;
+import ir.hamsaa.persiandatepicker.api.PersianPickerDate;
+import ir.hamsaa.persiandatepicker.api.PersianPickerListener;
+import ir.hamsaa.persiandatepicker.util.PersianCalendarUtils;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link AddInvoicesFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AddInvoicesFragment extends Fragment {
+public class AddInvoicesFragment extends Fragment implements View.OnClickListener, View.OnLongClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,6 +57,25 @@ public class AddInvoicesFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+
+    /// component and variable
+
+
+    private EditText edtPrice, edtTitle;
+    private TextView txtChooseDate, txtShowInvoice, txtPriceType, txtPriceTypeResult;
+    private NavController mNavController;
+    private StringBuilder spPrice = new StringBuilder();
+    private String titleLength, priceLength;
+    private PriceType priceType = new PriceType();
+    private int actionDate = 0; // 202221015
+    private ImageView imgInsertBack;
+    private String current = "";
+    private String invoicePrice, invoiceTitle, engDate, farsiDate, chooseDate = null;
+    private PriceType globalPriceType;
+
+    //edit
+
 
     public AddInvoicesFragment() {
         // Required empty public constructor
@@ -63,4 +114,256 @@ public class AddInvoicesFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_invoices, container, false);
     }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        intView(view);
+    }
+
+    private void intView(View view) {
+
+        mNavController = Navigation.findNavController(view);
+
+
+        imgInsertBack = view.findViewById(R.id.img_back);
+        edtPrice = view.findViewById(R.id.edt_price);
+        edtTitle = view.findViewById(R.id.edt_name);
+
+
+        txtPriceType = view.findViewById(R.id.txt_price_type);
+        txtPriceTypeResult = view.findViewById(R.id.txt_price_type_result);
+        txtShowInvoice = view.findViewById(R.id.txt_invoice_show);
+        txtChooseDate = view.findViewById(R.id.txt_date_chosse);
+
+        edtOnline();
+
+
+        imgInsertBack.setOnClickListener(this);
+        txtChooseDate.setOnClickListener(this);
+        txtPriceType.setOnClickListener(this);
+        txtPriceTypeResult.setOnLongClickListener(this);
+
+
+    }
+
+    private void edtOnline() {
+
+
+        edtPrice.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                OnlineInvoices();
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+                titleLength = editable.toString();
+                StateLive(titleLength, priceLength);
+                try {
+
+                    if (!editable.toString().equals(current)) {
+                        edtPrice.removeTextChangedListener(this);
+                        String cleanString = editable.toString().replaceAll("[$,.]]", "");
+                        double parsed = Double.parseDouble(cleanString.equals("") ? "0" : cleanString);
+                        String formatted = NumberFormat.getNumberInstance().format(parsed);
+                        current = formatted;
+                        edtPrice.setText(formatted);
+                        edtPrice.setSelection(formatted.length());
+                        edtPrice.addTextChangedListener(this);
+                        invoicePrice = formatted;
+
+                    }
+                } catch (Exception e) {
+
+                }
+            }
+        });
+
+        edtTitle.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+                priceLength = editable.toString();
+                StateLive(titleLength, priceLength);
+                invoiceTitle = editable.toString();
+
+            }
+        });
+
+
+    }
+
+
+    private void StateLive(String titleLength, String priceLength) {
+
+        if (priceLength == null || priceLength.equals("") || titleLength == null || titleLength.equals("")) {
+            imgInsertBack.setImageResource(ChangeIconAddExpenses(0)); // no invoice
+        } else imgInsertBack.setImageResource(ChangeIconAddExpenses(1));  // have invoice
+    }
+
+
+    private void OnlineInvoices() {
+        Spanned strHtml = Html.fromHtml(" شما در تاریخ  " + "<font color='red'>"
+                + engDate + "</font>" + " مبلغ  " + "<font color='red'>" + invoicePrice
+                + "</font>" + " بابت هزینه  " + "<font color='red'>" + invoiceTitle + "</font>" + " پرداخت کرده اید. ");
+
+        txtShowInvoice.setText(strHtml);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+
+            case R.id.img_back:
+                String tempTitle = edtTitle.getText().toString();
+                String tempPrice = edtPrice.getText().toString();
+
+                if (validation(tempTitle, tempPrice)) {
+                    /// AddNewPrice();
+                } else {
+                    Toast.makeText(getContext(), "Error Null !", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.txt_date_chosse:
+                PersianDataPicker();
+                break;
+            case R.id.txt_price_type:
+            //  OpenPriceTypeDialog();
+                break;
+        }
+
+    }
+
+    private void PersianDataPicker() {
+
+
+        String toDay = Utility.getIranianDateInt();
+
+        String[] setToday = toDay.split("/");
+
+
+        PersianDatePickerDialog picker = new PersianDatePickerDialog(getActivity())
+                .setPositiveButtonString("باشه")
+                .setNegativeButton("بیخیال")
+                .setTodayButton("امروز")
+                .setTodayButtonVisible(true)
+                .setMinYear(1300)
+                .setMaxYear(PersianDatePickerDialog.THIS_YEAR)
+                .setMaxMonth(PersianDatePickerDialog.THIS_MONTH)
+                .setMaxDay(PersianDatePickerDialog.THIS_DAY)
+                .setInitDate(Integer.parseInt(setToday[0]),
+                        Integer.parseInt(setToday[1]),
+                        Integer.parseInt(setToday[2]))
+                .setActionTextColor(Color.GRAY)
+                //.setTypeFace(typeface)
+                .setTitleType(PersianDatePickerDialog.WEEKDAY_DAY_MONTH_YEAR)
+                .setShowInBottomSheet(true)
+                .setListener(new PersianPickerListener() {
+                    @Override
+                    public void onDateSelected(@NotNull PersianPickerDate persianPickerDate) {
+                        Log.d(TAG, "onDateSelected: " + persianPickerDate.getTimestamp());//675930448000
+                        Log.d(TAG, "onDateSelected: " + persianPickerDate.getGregorianDate());//Mon Jun 03 10:57:28 GMT+04:30 1991
+                        Log.d(TAG, "onDateSelected: " + persianPickerDate.getPersianLongDate());// دوشنبه  13  خرداد  1370
+                        Log.d(TAG, "onDateSelected: " + persianPickerDate.getPersianMonthName());//خرداد
+                        Log.d(TAG, "onDateSelected: " + PersianCalendarUtils.isPersianLeapYear(persianPickerDate.getPersianYear()));//true
+
+
+                        Date selectedDate = persianPickerDate.getGregorianDate();
+                        DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+                        String strDate = dateFormat.format(selectedDate);
+
+
+                        DateFormat dateFormatDate = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
+
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.setTime(selectedDate);
+
+                        chooseDate = Utility.getIranianDateCustom(calendar);
+                        actionDate = Integer.parseInt(strDate);//20220105
+                        txtChooseDate.setText(persianPickerDate.getPersianLongDate());
+                        engDate = dateFormatDate.format(selectedDate);
+                        OnlineInvoices();
+
+                    }
+
+                    @Override
+                    public void onDismissed() {
+
+                    }
+                });
+
+        picker.show();
+    }
+
+    private boolean validation(String tempTitle, String tempPrice) {
+        if ((tempPrice == null || tempPrice.equals("")) || (tempTitle.equals("") || tempTitle == null)) {
+            return false;
+        }
+        return true;
+    }
+
+
+    @Override
+    public boolean onLongClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.txt_price_type_result:
+                removePriceType();
+                break;
+        }
+        return false;
+    }
+
+    private void removePriceType() {
+        txtPriceTypeResult.setVisibility(View.GONE);
+        txtPriceTypeResult.setText("");
+    }
+
+
+    private String GetFarsiDate(){
+        if (chooseDate==null) return Utility.getIranianDate();
+        else return chooseDate;
+    }
+
+    private String GetEngDate(){
+
+        Date date=new Date();
+        if (engDate==null){
+            return  DateFormat.getDateTimeInstance(DateFormat.DEFAULT,DateFormat.SHORT).format(date);
+
+        }else  return engDate;
+    }
+
+
+
+    private int GetActionDate(){
+        if (actionDate==0) return Utility.GetActionDate();
+        else return actionDate;
+    }
+
+
+
+
+
+
 }
